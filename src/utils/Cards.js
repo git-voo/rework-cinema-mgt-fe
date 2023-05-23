5c1d8a9e5988ac9ab90c65c14143f6963b9bb829
janoth123 <70852198+janoth123@users.noreply.github.com>
2023-05-23 18:23:56 +0100

    feat: card component created to handle cards

---
+++ src/utils/Cards.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div className="flex ">
+        <div>
+          <p>Sep</p>
+          <h3>13</h3>
+        </div>
+        <div>
+          <h3>Indonesia- Korea conference</h3>
+          <p>Seonanna, Daerah khusus Ibukota Yhogyakarta, Indonesia</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
