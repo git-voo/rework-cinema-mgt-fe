dac185eb640df78d8371ae2d88b1432fac7bb890
Onoja Victor <onoja.jsdev@gmail.com>
2023-06-03 15:26:24 +0100

    separate main Nav bar

---
+++ src/components/MainNavBar.js
@@ -0,0 +1,21 @@
+import logo from "../assets/images/EVENTBUX.png";
+
+
+export default function MainNavBar() {
+  return (
+    <div className="banner-bg">
+      <div className="nav_header_homepage">
+        <nav className="main-nav">
+          <div className="logo-section">
+            <img className="logo-image" src={logo} alt="logo" />
+          </div>
+          <div className="links-section">
+            {/* <button>Help center</button> */}
+            <button className="login">Login</button>
+            <button className="signup">Sign up</button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+}
