e18bc899c184ca20b7b44fe6e42a7f9dd7096323
Onoja Victor <onoja.jsdev@gmail.com>
2023-06-29 02:56:45 +0100

    render nav according to login status

--- src/components/MainNavBar.js
+++ src/components/MainNavBar.js
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/images/EVENTBUX.png";
+import { useContext } from "react";
+import { variableManager } from "../contexts/VariablesContext";
 
 
 export default function MainNavBar() {
   const navigate = useNavigate()
+  const {user} = useContext(variableManager)
   return (
     <div className="banner-bg main-navigation-container">
       <div className="nav_header_homepage">
@@ -11,11 +14,21 @@ export default function MainNavBar() {
           <div className="logo-section">
             <img className="logo-image" src={logo} alt="logo" />
           </div>
-          <div className="links-section">
-            {/* <button>Help center</button> */}
-            <button className="login" onClick={()=>navigate("/auth/login")} >Login</button>
-            <button className="signup" onClick={()=>navigate("/auth/register")}>Sign up</button>
-          </div>
+
+          {
+            user?(
+              <div className="links-section"> 
+              <button className="login" onClick={()=>navigate("/auth/login")} >Profile</button>
+              <button className="signup" onClick={()=>navigate("/auth/register")}>Logout</button>
+            </div>
+            ):( 
+              <div className="links-section"> 
+              <button className="login" onClick={()=>navigate("/auth/login")} >Login</button>
+              <button className="signup" onClick={()=>navigate("/auth/register")}>Sign up</button>
+            </div>
+            )
+          }
+         
         </nav>
       </div>
     </div>
