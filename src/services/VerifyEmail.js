0075ea4f0be0bbce8d3fe0569aca0e4329290fd0
Onoja Victor <onoja.jsdev@gmail.com>
2023-06-29 02:56:26 +0100

    add email/code verification functions

---
+++ src/services/VerifyEmail.js
@@ -0,0 +1,18 @@
+import { axiosInstance } from "./AxiosInstance";
+
+export async function verifyEmail(payload) {
+  if (!payload) return false;
+  let { data, error } = "";
+  try {
+    await axiosInstance.post("/auth/verify", payload).then((resp) => {
+      data = resp.data;
+    });
+  } catch (err) {
+    error = err;
+  }
+
+  return { data, error };
+}
+
+// Use this to verify any email and code sent to it. 
+// two parameters are required, the email, and the code received by user
